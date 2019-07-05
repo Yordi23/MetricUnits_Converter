783e6e29b9f1e5a4ba21bc78fbb42fcbf6ec4f05
Yordi23 <39386472+Yordi23@users.noreply.github.com>
2019-07-05 18:12:19 -0400

    Solution

---
+++ MetricUnits_Converter/MetricUnits_Converter/Converter.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MetricUnits_Converter
+{
+    public class InvalidFileFormatException : Exception { public InvalidFileFormatException(string message) : base(message) { } };
+
+    public class Converter
+    {
+        string[] data;
+        string path;
+
+        public string[] Convert(string path)
+        {
+            data = File.ReadAllLines(path);
+            this.path = path;
+
+            for (int i=0; i< data.Length; i++)
+            {
+                string[] line = data[i].Split('\t');
+                if (line.Length != 3) throw new InvalidFileFormatException("The file with the input data does not have the expected amount of columns");
+                data[i] += "\t" + Calculate(line);
+            }
+
+            return data;
+        }
+
+        private double Calculate(string[] line)
+        { /*
+            Kilometer   =  km
+            Hectometer  =  hm
+            Decameter   =  dam
+            Meter       =  m
+            Decimeter   =  dm
+            Centimeter  =  cm
+            Millimeter  =  mm
+          */
+
+            string[] metricUnits = { "km", "hm", "dam", "m", "dm", "cm", "mm"};
+            int from = -1, to = -1, remainder = 0;
+            double result;
+            try {  result = double.Parse(line[0]); }
+
+            catch (Exception e)
+            {
+                throw new InvalidFileFormatException("The file with the input data does not have a valid format");
+            }
+
+            for (int i = 0; i < metricUnits.Length; i++)
+            {
+                if (line[1] == metricUnits[i]) from = i;
+                if (line[2] == metricUnits[i]) to = i;
+            }
+
+            if (from == -1 || to == -1 ) throw new InvalidFileFormatException("The file with the input data contains an invalid Metric Unit");
+
+            remainder = from - to;
+
+            if (remainder== 0) return double.Parse(line[0]);
+
+            if (remainder < 0)
+            {
+                for (int i = 0; i < Math.Abs(remainder); i++)
+                    result *= 10;
+            }
+            else if (remainder > 0)
+            {
+                for (int i = 0; i < Math.Abs(remainder); i++)
+                    result /= 10;
+            }
+
+            return result;
+        }
+
+        private string TrimPath(string path)
+        {
+            for (int i = path.Length - 1; ; i--)
+                if (path[i] == '\\') return path.Remove(i);         
+        }
+
+        public void WriteOutput()
+        {
+            File.WriteAllLines(TrimPath(this.path) + "\\Output.txt", data);
+        }
+
+
+    }
+}
---
+++ MetricUnits_Converter/UnitTests/Tests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using MetricUnits_Converter;
+using System.IO;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        Converter converter = new Converter();
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(File.ReadAllLines(@"Tests\Test 1\Expected.txt"), converter.Convert(@"Tests\Test 1\Convert.txt"));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(File.ReadAllLines(@"Tests\Test 2\Expected.txt"), converter.Convert(@"Tests\Test 2\Convert.txt"));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(File.ReadAllLines(@"Tests\Test 3\Expected.txt"), converter.Convert(@"Tests\Test 3\Convert.txt"));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            InvalidFileFormatException exception = Assert.Throws<InvalidFileFormatException>(() => converter.Convert(@"Tests\Test 4\Convert.txt"));
+            Assert.AreEqual(exception.Message, "The file with the input data does not have the expected amount of columns");
+        }
+
+        [Test]
+        public void Test5()
+        {
+            InvalidFileFormatException exception = Assert.Throws<InvalidFileFormatException>(() => converter.Convert(@"Tests\Test 5\Convert.txt"));
+            Assert.AreEqual(exception.Message, "The file with the input data does not have a valid format");
+        }
+
+        [Test]
+        public void Test6()
+        {
+            InvalidFileFormatException exception = Assert.Throws<InvalidFileFormatException>(() => converter.Convert(@"Tests\Test 6\Convert.txt"));
+            Assert.AreEqual(exception.Message, "The file with the input data contains an invalid Metric Unit");
+        }
+    }
+}
